30b83d497c1a4115867d50a14b27b4980e8b92ff
dymafr <contact@dyma.fr>
2023-03-09 14:59:56 +0100

    maj

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,25 @@
 <script setup lang="ts">
-import TheHeader from './components/Header.vue';
-import TheFooter from './components/Footer.vue';
-import Boutique from './features/boutique/Boutique.vue';
-import Admin from './features/admin/Admin.vue';
-import { reactive, type Component as C } from 'vue';
-import type { Page } from './interfaces';
-import { seed, seed40articles } from './data/seed';
+import TheHeader from "./components/Header.vue";
+import TheFooter from "./components/Footer.vue";
+import Boutique from "./features/boutique/Boutique.vue";
+import Admin from "./features/admin/Admin.vue";
+import { reactive, type Component as C } from "vue";
+import type { Page } from "./interfaces";
+import { seed, seed40articles } from "./data/seed";
 
 const state = reactive<{
-    page: Page;
+  page: Page;
 }>({
-    page: 'Boutique'
+  page: "Boutique",
 });
 
-const pages: { [s: string]: C; } = {
-    Boutique,
-    Admin
+const pages: { [s: string]: C } = {
+  Boutique,
+  Admin,
 };
 
 function navigate(page: Page): void {
-    state.page = page;
+  state.page = page;
 }
 
 // seed('vueprojectproducts');
@@ -27,37 +27,37 @@ function navigate(page: Page): void {
 </script>
 
 <template>
-    <div class="app-container">
-        <TheHeader @navigate="navigate" :page="state.page" class="header" />
-        <div class="app-content">
-            <Suspense>
-                <Component :is="pages[state.page]" />
-            </Suspense>
-        </div>
-        <TheFooter class="footer hide-xs" />
+  <div class="app-container">
+    <TheHeader @navigate="navigate" :page="state.page" class="header" />
+    <div class="app-content">
+      <Suspense>
+        <Component :is="pages[state.page]" />
+      </Suspense>
     </div>
+    <TheFooter class="footer hide-xs" />
+  </div>
 </template>
 
 <style lang="scss">
-@import "./assets/scss/base.scss";
-@import "./assets/scss/debug.scss";
+@use "./assets/scss/base.scss" as *;
+@use "./assets/scss/debug.scss" as *;
 
 .app-container {
-    height: 100vh;
-    display: grid;
-    grid-template-areas: "header" "app-content" "footer";
-    grid-template-rows: 48px auto 48px;
+  height: 100vh;
+  display: grid;
+  grid-template-areas: "header" "app-content" "footer";
+  grid-template-rows: 48px auto 48px;
 }
 
 .header {
-    grid-area: header;
+  grid-area: header;
 }
 
 .app-content {
-    grid-area: app-content;
+  grid-area: app-content;
 }
 
 .footer {
-    grid-area: footer;
+  grid-area: footer;
 }
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,142 +1,152 @@
-<script setup lang="ts">import type { Page } from '@/interfaces';
-import { reactive } from 'vue';
-import Calc from './Calc.vue';
+<script setup lang="ts">
+import type { Page } from "@/interfaces";
+import { reactive } from "vue";
+import Calc from "./Calc.vue";
 
 const state = reactive<{
-    open: boolean
+  open: boolean;
 }>({
-    open: false
-})
+  open: false,
+});
 
 defineProps<{
-    page: Page
-}>()
+  page: Page;
+}>();
 
 const emit = defineEmits<{
-    (e: 'navigate', page: Page): void
-}>()
-
+  (e: "navigate", page: Page): void;
+}>();
 </script>
 
-
 <template>
-    <header class="px-20 d-flex flex-row align-items-center">
-        <a href="#" class="d-flex flex-row align-items-center mr-20">
-            <img src="../assets/logo.svg" />
-            <span class="logo">Dyma</span>
-        </a>
-        <div class="d-flex flex-row align-items-center flex-fill actions-container">
-            <ul class="d-flex flex-row flex-fill hide-xs flex-fill">
-                <li class="mr-10">
-                    <a
-                        :class="{ active: page === 'Boutique' }"
-                        @click="emit('navigate', 'Boutique')"
-                    >Boutique</a>
-                </li>
-                <li>
-                    <a
-                        :class="{ active: page === 'Admin' }"
-                        @click="emit('navigate', 'Admin')"
-                    >Admin</a>
-                </li>
-            </ul>
-            <ul class="d-flex flex-row hide-xs">
-                <li class="mr-10">
-                    <a href="#">Inscription</a>
-                </li>
-                <li>
-                    <a href="#">Connexion</a>
-                </li>
-            </ul>
-            <div class="menu-xs-container">
-                <Calc :open="state.open" @close="state.open = false" :transparent="true" />
-                <i @click="state.open = !state.open" class="fa-solid fa-bars show-xs"></i>
-                <Transition>
-                    <ul @click="state.open = false" v-if="state.open" class="menu card">
-                        <li>
-                            <a
-                                :class="{ active: page === 'Boutique' }"
-                                @click="emit('navigate', 'Boutique')"
-                            >Boutique</a>
-                        </li>
-                        <li>
-                            <a
-                                :class="{ active: page === 'Admin' }"
-                                @click="emit('navigate', 'Admin')"
-                            >Admin</a>
-                        </li>
-                        <li>
-                            <a href="#">Inscription</a>
-                        </li>
-                        <li>
-                            <a href="#">Connexion</a>
-                        </li>
-                    </ul>
-                </Transition>
-            </div>
-        </div>
-    </header>
+  <header class="px-20 d-flex flex-row align-items-center">
+    <a href="#" class="d-flex flex-row align-items-center mr-20">
+      <img src="../assets/logo.svg" />
+      <span class="logo">Dyma</span>
+    </a>
+    <div class="d-flex flex-row align-items-center flex-fill actions-container">
+      <ul class="d-flex flex-row flex-fill hide-xs flex-fill">
+        <li class="mr-10">
+          <a
+            :class="{ active: page === 'Boutique' }"
+            @click="emit('navigate', 'Boutique')"
+            >Boutique</a
+          >
+        </li>
+        <li>
+          <a
+            :class="{ active: page === 'Admin' }"
+            @click="emit('navigate', 'Admin')"
+            >Admin</a
+          >
+        </li>
+      </ul>
+      <ul class="d-flex flex-row hide-xs">
+        <li class="mr-10">
+          <a href="#">Inscription</a>
+        </li>
+        <li>
+          <a href="#">Connexion</a>
+        </li>
+      </ul>
+      <div class="menu-xs-container">
+        <Calc
+          :open="state.open"
+          @close="state.open = false"
+          :transparent="true"
+        />
+        <i
+          @click="state.open = !state.open"
+          class="fa-solid fa-bars show-xs"
+        ></i>
+        <Transition>
+          <ul @click="state.open = false" v-if="state.open" class="menu card">
+            <li>
+              <a
+                :class="{ active: page === 'Boutique' }"
+                @click="emit('navigate', 'Boutique')"
+                >Boutique</a
+              >
+            </li>
+            <li>
+              <a
+                :class="{ active: page === 'Admin' }"
+                @click="emit('navigate', 'Admin')"
+                >Admin</a
+              >
+            </li>
+            <li>
+              <a href="#">Inscription</a>
+            </li>
+            <li>
+              <a href="#">Connexion</a>
+            </li>
+          </ul>
+        </Transition>
+      </div>
+    </div>
+  </header>
 </template>
 
 <style lang="scss" scoped>
-@use '@/assets/scss/mixins';
+@use "@/assets/scss/mixins";
 
 header {
-    background-color: var(--primary-1);
+  background-color: var(--primary-1);
+  a {
+    color: var(--text-primary-color);
+    img {
+      width: 20px;
+      margin-right: 5px;
+    }
+    .logo {
+      font-weight: 700;
+      font-size: 20px;
+    }
+  }
+  i {
+    @include mixins.sm {
+      display: none;
+    }
+    justify-self: end;
+    color: white;
+    font-size: 20px;
+    cursor: pointer;
+  }
+  a.active {
+    text-decoration: underline;
+  }
+  .actions-container {
+    @include mixins.xs {
+      justify-content: end;
+    }
+  }
+
+  .menu-xs-container {
+    position: relative;
+  }
+
+  .menu {
+    z-index: 2;
+    position: absolute;
+    top: 20px;
+    right: 0px;
+    li {
+      padding: 10px;
+    }
     a {
-        color: var(--text-primary-color);
-        img {
-            width: 20px;
-            margin-right: 5px;
-        }
-        .logo {
-            font-weight: 700;
-            font-size: 20px;
-        }
+      color: var(--text-color);
     }
-    i {
-        @include mixins.sm {
-            display: none;
-        }
-        justify-self: end;
-        color: white;
-        font-size: 20px;
-        cursor: pointer;
-    }
-    a.active {
-        text-decoration: underline;
-    }
-    .actions-container {
-        @include mixins.xs {
-            justify-content: end;
-        }
-    }
-
-    .menu-xs-container {
-        position: relative;
-    }
-
-    .menu {
-        z-index: 2;
-        position: absolute;
-        top: 20px;
-        right: 0px;
-        li {
-            padding: 10px;
-        }
-        a {
-            color: var(--text-color);
-        }
-    }
+  }
 }
 .v-leave-to,
 .v-enter-from {
-    transform: translateY(-10px);
-    opacity: 0;
+  transform: translateY(-10px);
+  opacity: 0;
 }
 
 .v-leave-active,
 .v-enter-active {
-    transition: all 0.2s;
+  transition: all 0.2s;
 }
 </style>
